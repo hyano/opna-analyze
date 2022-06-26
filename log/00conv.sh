ba318a447e306f00cffb2c99651755a1c1615dd6
hyano <1297631+hyano@users.noreply.github.com>
2022-06-26 11:24:25 +0900

    ロジアナのログの波形化を再実行した

---
+++ log/00conv.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+PCMBUSY="
+adpcm_play_pcmbusy
+adpcm_play2_pcmbusy
+adpcm_play_limit_pcmbusy
+"
+
+MDEN="
+adpcm_play_mden
+adpcm_play2_mden
+adpcm_play_limit2_mden
+adpcm_play_limit3_mden
+adpcm_play_limit4_mden
+adpcm_play_limit5_mden
+adpcm_play_limit_mden
+
+mem_rw_mden
+mem_rw2_mden
+mem_rw3_mden
+mem_rw4_mden
+"
+
+
+conv()
+{
+    logs=$1
+    opt=$2
+    for l in $logs
+    do
+        echo $l
+        cd $l
+        bzcat $l.bin.bz2 | ../../logic-analyzer/towav $opt -o $l
+        cd ..
+    done
+}
+
+conv "$PCMBUSY" "-s"
+conv "$MDEN" "-m"
